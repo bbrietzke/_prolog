ecaf0b5394ee61dbdaada2c0dcf8311925b6e0fa
Brian Brietzke <brian_brietzke@comcast.com>
2018-12-21 07:55:19 -0600

    using headers and source files correctly.

---
+++ src/utils.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include "define_m0.h"
+#include "wam_m0.h"
+
+void dump_heap(HEAP* h) {
+    int index = h->index;
+    int i = 0;
+
+    while(i < index) {
+        printf("%10d : ", i);
+        switch(h->cells[i].variable.tag) {
+            case REF:
+                printf("REF :: %d\n", h->cells[i].variable.address);
+                break;
+            case STR: 
+                printf("STR :: %d\n", h->cells[i].variable.address);
+                break;
+            default:
+                printf("functor %s/%d\n", h->cells[i].structure.name, h->cells[i].structure.airty);
+        }
+
+        i = i + 1;
+    }
+}
--- src/utils.h
+++ src/utils.h
@@ -5,26 +5,4 @@
 
 #pragma once
 
-void dump_heap(HEAP* h) {
-    int index = h->index;
-    int i = 0;
-
-    while(i < index) {
-        printf("%10d : ", i);
-        switch(h->cells[i].variable.tag) {
-            case REF:
-                printf("REF :: %d\n", h->cells[i].variable.address);
-                break;
-            case STR: 
-                printf("STR :: %d\n", h->cells[i].variable.address);
-                break;
-            case V: 
-                printf("VAR :: %d\n", h->cells[i].variable.address);
-                break;
-            default:
-                printf("functor %s/%d\n", h->cells[i].structure.name, h->cells[i].structure.airty);
-        }
-
-        i = i + 1;
-    }
-}
+void dump_heap(HEAP* h);
